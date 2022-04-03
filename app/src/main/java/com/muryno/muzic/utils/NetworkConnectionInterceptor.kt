77c7d214a0b6cc2e1a97fac7852431f510250c4b
gbolahan146 <gbolahanoduyemi1@gmail.com>
2022-04-03 12:38:31 +0100

    chore: update network connection intercerptor to be strongly typed

--- app/src/main/java/com/muryno/muzic/utils/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/muryno/muzic/utils/NetworkConnectionInterceptor.kt
@@ -4,41 +4,41 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.annotation.RequiresApi
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class NetworkConnectionInterceptor(context: Context) : Interceptor {
-    private val applicationContext: Context = context.applicationContext;
+/**
+ * Interceptor that checks if the device has an active internet connection.
+ *
+ * @property applicationContext the application context
+ */
+class NetworkConnectionInterceptor(private val applicationContext: Context) : Interceptor {
 
+    /**
+     * Intercepts the [chain] and throws a [NoInternetException] if there is no active internet connection.
+     *
+     * @param chain the chain of interceptors
+     * @return the response
+     * @throws NoInternetException if there is no active internet connection
+     */
     @RequiresApi(Build.VERSION_CODES.M)
     override fun intercept(chain: Interceptor.Chain): Response {
-        if (isNetworkAvailable() == 0) {
+        if (!isNetworkAvailable()) {
             throw NoInternetException("Ensure you have an active internet connection")
         } else {
             return chain.proceed(chain.request())
         }
-
     }
+
+    /**
+     * Checks if the device has an active internet connection.
+     *
+     * @return true if the device has an active internet connection, false otherwise
+     */
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun isNetworkAvailable(): Int {
-        var result = 0 // Returns connection type. 0: none; 1: mobile data; 2: wifi
-        val cm =
-            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
-        val networkCapabilities = cm?.getNetworkCapabilities(cm.activeNetwork)
-        if (networkCapabilities != null) {
-            when {
-                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
-                    result = 2
-                }
-                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
-                    result = 1
-                }
-                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> {
-                    result = 3
-                }
-            }
-        }
-        return result
+    private fun isNetworkAvailable(): Boolean {
+        val cm = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = cm.activeNetwork?.let { cm.getNetworkCapabilities(it) }
+        return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
     }
 }
