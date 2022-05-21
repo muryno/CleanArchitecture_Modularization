68af04a05b0134255a2724f3a88e88751f681282
gbolahan146 <gbolahanoduyemi1@gmail.com>
2022-05-21 09:29:23 +0100

    add country to viewholder

--- artist-ui/src/main/java/com/muryno/artist/artist/adapter/ArtistAdapter.kt
+++ artist-ui/src/main/java/com/muryno/artist/artist/adapter/ArtistAdapter.kt
@@ -49,6 +49,7 @@ class ArtistAdapter:
         RecyclerView.ViewHolder(itemView) {
         private val txtName: TextView  by lazy { itemView.findViewById(R.id.txt_name) }
         private val txtCity: TextView by lazy { itemView.findViewById(R.id.txt_city) }
+        private val txtCountry: TextView by lazy { itemView.findViewById(R.id.txt_country) }
         private val txtGender: TextView by lazy { itemView.findViewById(R.id.txt_gender) }
         private val txtDescriptionLabel: TextView by lazy { itemView.findViewById(R.id.txt_description_label) }
 
@@ -57,6 +58,7 @@ class ArtistAdapter:
             txtName.text = item.name
             txtCity.text = item.city
             txtGender.text = item.gender
+            txtCountry.text = item.country
             txtDescriptionLabel.text = item.description
             itemView.setOnClickListener {
                 clickedArtist(item)
