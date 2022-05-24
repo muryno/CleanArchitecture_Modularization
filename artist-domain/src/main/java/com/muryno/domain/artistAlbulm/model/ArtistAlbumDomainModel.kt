3d36a33c6f7299f9c63b8f3dd0aa6a785d918711
gbolahan146 <gbolahanoduyemi1@gmail.com>
2022-05-24 09:34:47 +0100

    add label to album domain model

--- artist-domain/src/main/java/com/muryno/domain/artistAlbulm/model/ArtistAlbumDomainModel.kt
+++ artist-domain/src/main/java/com/muryno/domain/artistAlbulm/model/ArtistAlbumDomainModel.kt
@@ -5,6 +5,7 @@ data class ArtistAlbumDomainModel(
     val primaryType: String,
     val genre: String,
     val releaseDate: String,
+    val label: String,
     val title: String,
     val shortDescription: String,
     val fullDescription: String,
