2eb2554e36ed99e36b191209f8428b780f6e36bc
gbolahan146 <gbolahanoduyemi1@gmail.com>
2022-05-25 09:38:11 +0100

    add short desc to album data model

--- artist-data/src/main/java/com/muryno/data/model/ArtistAlbumDataModel.kt
+++ artist-data/src/main/java/com/muryno/data/model/ArtistAlbumDataModel.kt
@@ -4,6 +4,7 @@ data class ArtistAlbumDataModel(
     val primaryType: String,
     val genre: String,
     val label: String,
+    val shortDescription: String,
     val releaseDate: String,
     val title: String,
     val id: String,
