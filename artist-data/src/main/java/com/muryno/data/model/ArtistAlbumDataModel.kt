6f657ec65c120ff4bea96438232b39618c36f43e
gbolahan146 <gbolahanoduyemi1@gmail.com>
2022-05-25 09:37:53 +0100

    add label to album data model

--- artist-data/src/main/java/com/muryno/data/model/ArtistAlbumDataModel.kt
+++ artist-data/src/main/java/com/muryno/data/model/ArtistAlbumDataModel.kt
@@ -3,6 +3,7 @@ package com.muryno.data.model
 data class ArtistAlbumDataModel(
     val primaryType: String,
     val genre: String,
+    val label: String,
     val releaseDate: String,
     val title: String,
     val id: String,
