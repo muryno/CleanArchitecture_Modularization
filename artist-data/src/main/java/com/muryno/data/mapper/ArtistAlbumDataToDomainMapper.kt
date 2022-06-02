7ffb487ef943769545bdb0b01d773f083dd6a902
gbolahan146 <gbolahanoduyemi1@gmail.com>
2022-06-02 20:06:41 +0100

    updates

--- artist-data/src/main/java/com/muryno/data/mapper/ArtistAlbumDataToDomainMapper.kt
+++ artist-data/src/main/java/com/muryno/data/mapper/ArtistAlbumDataToDomainMapper.kt
@@ -4,6 +4,7 @@ import com.muryno.data.model.ArtistAlbumDataModel
 import com.muryno.domain.artistAlbulm.model.ArtistAlbumDomainModel
 
 class ArtistAlbumDataToDomainMapper {
+
     fun toDomain(input: ArtistAlbumDataModel): ArtistAlbumDomainModel {
         return ArtistAlbumDomainModel(
             id = input.id,
