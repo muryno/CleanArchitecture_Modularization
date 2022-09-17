276e5b199c009e5af13ac96c43e8b1045d79a1da
gbolahan146 <gbolahanoduyemi1@gmail.com>
2022-09-17 06:46:36 +0100

    base fragment updates

--- artist-ui/src/main/java/com/muryno/artist/base/BaseFragment.kt
+++ artist-ui/src/main/java/com/muryno/artist/base/BaseFragment.kt
@@ -9,30 +9,30 @@ import android.view.inputmethod.InputMethodManager
 import androidx.annotation.CallSuper
 import androidx.fragment.app.Fragment
 import com.muryno.presention.architecture.viewmodel.BaseViewModel
+
 private const val NO_LAYOUT_RESOURCE = 0
+
 abstract class BaseFragment<VIEW_STATE : Any> : Fragment() {
     protected abstract val viewModel: BaseViewModel<VIEW_STATE>
     open val layoutResourceId: Int = NO_LAYOUT_RESOURCE
     @CallSuper
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        val view = if (layoutResourceId != NO_LAYOUT_RESOURCE) {
-            inflater.inflate(layoutResourceId, container, false).apply {
-                bindViews()
-            }
-        } else {
-            null
-        }
+        val view =
+                if (layoutResourceId != NO_LAYOUT_RESOURCE) {
+                    inflater.inflate(layoutResourceId, container, false).apply { bindViews() }
+                } else {
+                    null
+                }
         return view
     }
 
     abstract fun View.bindViews()
     protected fun closeSoftKeyboard() {
-        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE)
-                as InputMethodManager
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
