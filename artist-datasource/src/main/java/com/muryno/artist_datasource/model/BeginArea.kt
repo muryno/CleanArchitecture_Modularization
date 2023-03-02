125d35a4881fc76f63cec31fb5a1509b2cca5f93
Muraino Yakubu <murainoy@yahoo.com>
2023-03-02 12:33:29 +0000

    updated sortName to camel case

--- artist-datasource/src/main/java/com/muryno/artist_datasource/model/BeginArea.kt
+++ artist-datasource/src/main/java/com/muryno/artist_datasource/model/BeginArea.kt
@@ -5,10 +5,10 @@ import com.google.gson.annotations.SerializedName
 data class BeginArea(
     val id: String,
     @SerializedName("life-span")
-    val life_span: LifeSpan,
+    val lifeSpan: LifeSpan,
     val name: String,
     @SerializedName("sort-name")
-    val sort_name: String,
+    val sortName: String,
     val type: String,
     @SerializedName("type-id")
     val type_id: String
