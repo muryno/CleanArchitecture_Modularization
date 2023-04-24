02937afa31462130fd9f993cb3f08de157c32c0a
Muraino Yakubu <murainoy@yahoo.com>
2023-04-24 11:12:55 +0100

    updated gradle version

--- buildSrc/src/main/java/BuildAndroidConfig.kt
+++ buildSrc/src/main/java/BuildAndroidConfig.kt
@@ -9,9 +9,9 @@ object BuildAndroidConfig {
 
     const val APPLICATION_ARTIST_UI = "com.muryno.artist"
 
-    const val COMPILE_SDK_VERSION = 33
+    const val COMPILE_SDK_VERSION = 34
     const val MIN_SDK_VERSION = 21
-    const val TARGET_SDK_VERSION = 33
+    const val TARGET_SDK_VERSION = 34
 
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
