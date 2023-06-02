0182b8ca9e4fea6f096683ed25ec464fbb93daf9
Muraino Yakubu <murainoy@yahoo.com>
2023-06-02 13:56:37 +0100

    reformating

--- artist-ui/build.gradle.kts
+++ artist-ui/build.gradle.kts
@@ -23,8 +23,6 @@ android {
         viewBinding = true
         compose = true
     }
-
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
