55bcdbf6c39562b24fa5ab7644d2fee28297e13c
Muraino Yakubu <murainoy@yahoo.com>
2023-06-11 12:13:41 +0100

    optimizing ArtistPresentationToUIMapper

--- artist-ui/src/main/java/com/muryno/artist/artist/mapper/ArtistPresentationToUIMapper.kt
+++ artist-ui/src/main/java/com/muryno/artist/artist/mapper/ArtistPresentationToUIMapper.kt
@@ -3,7 +3,6 @@ package com.muryno.artist.artist.mapper
 
 import com.muryno.artist.artist.model.ArtistUIModel
 import com.muryno.presention.artist.model.ArtistPresentationModel
-
 class ArtistPresentationToUIMapper {
     fun toUi(input: ArtistPresentationModel): ArtistUIModel {
         return ArtistUIModel(
