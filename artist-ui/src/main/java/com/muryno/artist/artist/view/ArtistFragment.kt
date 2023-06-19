623b9c03140c928cd84dfab27221cd3ec486e621
Muraino Yakubu <murainoy@yahoo.com>
2023-06-19 11:13:05 +0100

    optimizing ArtistFragment

--- artist-ui/src/main/java/com/muryno/artist/artist/view/ArtistFragment.kt
+++ artist-ui/src/main/java/com/muryno/artist/artist/view/ArtistFragment.kt
@@ -45,7 +45,6 @@ class ArtistFragment : BaseFragment<ArtistViewState>(){
         }
     }
 
-
     override fun onResume() {
         super.onResume()
         viewModel.viewState.observe(viewLifecycleOwner){viewState->
