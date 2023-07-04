abd5179a2d507698edd6fca8e42113804a28ce08
Muraino Yakubu <murainoy@yahoo.com>
2023-07-04 09:30:46 +0100

    created AltimeterDiagnostic

---
+++ app/src/main/java/com/muryno/muzic/component/diagnostics/AltimeterDiagnostic.kt
@@ -0,0 +1,19 @@
+package com.muryno.muzic.component.diagnostics
+
+import android.content.Context
+import com.kylecorry.trail_sense.diagnostics.DiagnosticCode
+import com.kylecorry.trail_sense.diagnostics.IDiagnostic
+import com.kylecorry.trail_sense.shared.UserPreferences
+
+class AltimeterDiagnostic(private val context: Context) :
+    IDiagnostic {
+
+    override fun scan(): List<DiagnosticCode> {
+        val prefs = UserPreferences(context)
+        if (prefs.altimeterMode == UserPreferences.AltimeterMode.Override) {
+            return listOf(DiagnosticCode.AltitudeOverridden)
+        }
+
+        return emptyList()
+    }
+}
