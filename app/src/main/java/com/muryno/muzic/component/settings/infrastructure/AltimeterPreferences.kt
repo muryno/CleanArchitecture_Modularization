0aff768b1244410ebf7ac45369a0d826a9c2de5e
Muraino Yakubu <murainoy@yahoo.com>
2023-07-05 19:30:57 +0100

    created AltimeterDiagnostic

---
+++ app/src/main/java/com/muryno/muzic/component/settings/infrastructure/AltimeterPreferences.kt
@@ -0,0 +1,24 @@
+package com.muryno.muzic.component.settings.infrastructure
+
+import android.content.Context
+import com.kylecorry.andromeda.preferences.BooleanPreference
+import com.kylecorry.andromeda.preferences.DurationPreference
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.settings.infrastructure.PreferenceRepo
+import java.time.Duration
+
+class AltimeterPreferences(context: Context) : PreferenceRepo(context) {
+
+    val useFusedAltimeterContinuousCalibration by BooleanPreference(
+        cache,
+        context.getString(R.string.pref_altimeter_continuous_calibration),
+        true
+    )
+
+    var fusedAltimeterForcedRecalibrationInterval by DurationPreference(
+        cache,
+        context.getString(R.string.pref_altimeter_forced_recalibration_interval),
+        Duration.ofHours(2)
+    )
+
+}
