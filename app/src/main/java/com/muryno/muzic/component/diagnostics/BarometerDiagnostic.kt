a102f2eaf432183d1a6271d3a51d30d016115a3a
Muraino Yakubu <murainoy@yahoo.com>
2023-07-07 09:31:39 +0100

    created BarometerDiagnostic

---
+++ app/src/main/java/com/muryno/muzic/component/diagnostics/BarometerDiagnostic.kt
@@ -0,0 +1,31 @@
+package com.muryno.muzic.component.diagnostics
+
+import android.content.Context
+import androidx.lifecycle.LifecycleOwner
+import com.kylecorry.andromeda.core.sensors.Quality
+import com.kylecorry.andromeda.sense.Sensors
+import com.kylecorry.andromeda.sense.barometer.IBarometer
+import com.kylecorry.trail_sense.diagnostics.BaseSensorQualityDiagnostic
+import com.kylecorry.trail_sense.diagnostics.DiagnosticCode
+import com.kylecorry.trail_sense.shared.sensors.SensorService
+
+class BarometerDiagnostic(context: Context, lifecycleOwner: LifecycleOwner?) :
+    BaseSensorQualityDiagnostic<IBarometer>(
+        context,
+        lifecycleOwner,
+        SensorService(context).getBarometer()
+    ) {
+
+    override fun scan(): List<DiagnosticCode> {
+        if (!Sensors.hasBarometer(context)) {
+            return listOf(DiagnosticCode.BarometerUnavailable)
+        }
+
+        if (canRun && sensor!!.quality == Quality.Poor) {
+            return listOf(DiagnosticCode.BarometerPoor)
+        }
+
+        return emptyList()
+    }
+
+}
