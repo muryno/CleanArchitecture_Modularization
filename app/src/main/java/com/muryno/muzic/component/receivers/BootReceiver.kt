2bd24b7a75df58436de387b2972a3d28b1988235
Muraino Yakubu <murainoy@yahoo.com>
2023-07-13 11:23:14 +0100

    created BootReceiver

---
+++ app/src/main/java/com/muryno/muzic/component/receivers/BootReceiver.kt
@@ -0,0 +1,16 @@
+package com.muryno.muzic.component.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.kylecorry.trail_sense.receivers.TrailSenseServiceUtils
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED && context != null) {
+            Log.d("BootReceiver", "Boot completed")
+            TrailSenseServiceUtils.restartServices(context, isInBackground = true)
+        }
+    }
+}
