0f272e7c8c34d83c78211a0d286db80e080ff7b9
Muraino Yakubu <murainoy@yahoo.com>
2023-07-19 17:23:36 +0100

    implemented CameraSettingsFragment

---
+++ app/src/main/java/com/muryno/muzic/component/ui/CameraSettingsFragment.kt
@@ -0,0 +1,28 @@
+package com.muryno.muzic.component.ui
+
+import android.os.Bundle
+import com.kylecorry.andromeda.fragments.AndromedaPreferenceFragment
+import com.kylecorry.trail_sense.R
+import com.muryno.muzic.component.settings.infrastructure.CameraPreferences
+
+class CameraSettingsFragment : AndromedaPreferenceFragment() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.camera_preferences, rootKey)
+
+        // Set the projection type options
+        val names = mapOf(
+            CameraPreferences.ProjectionType.EstimatedIntrinsics to "${getString(R.string.projection_estimated_intrinsics)} (${
+                getString(R.string.default_string)
+            })",
+            CameraPreferences.ProjectionType.ManufacturerIntrinsics to getString(R.string.projection_manufacturer_intrinsics),
+            CameraPreferences.ProjectionType.Perspective to getString(R.string.projection_perspective),
+            CameraPreferences.ProjectionType.Linear to getString(R.string.projection_linear)
+        )
+        val ids = names.map { it.key.id }
+
+        val projectionType = list(R.string.pref_augmented_reality_mapper)
+        projectionType?.entries = names.values.toTypedArray()
+        projectionType?.entryValues = ids.toTypedArray()
+    }
+}
