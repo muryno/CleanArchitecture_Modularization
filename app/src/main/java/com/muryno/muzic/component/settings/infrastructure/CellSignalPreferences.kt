cc2546f5d477eda999eb1735dd10b05eb9caa0af
Muraino Yakubu <murainoy@yahoo.com>
2023-07-20 19:54:47 +0100

    implemented CellSignalPreferences

---
+++ app/src/main/java/com/muryno/muzic/component/settings/infrastructure/CellSignalPreferences.kt
@@ -0,0 +1,17 @@
+package com.muryno.muzic.component.settings.infrastructure
+
+import android.content.Context
+import com.kylecorry.andromeda.preferences.BooleanPreference
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.shared.preferences.PreferencesSubsystem
+
+class CellSignalPreferences(private val context: Context) {
+    private val cache by lazy { PreferencesSubsystem.getInstance(context).preferences }
+
+    val populateCache by BooleanPreference(
+        cache,
+        context.getString(R.string.pref_cell_signal_refresh_cache),
+        true
+    )
+
+}
