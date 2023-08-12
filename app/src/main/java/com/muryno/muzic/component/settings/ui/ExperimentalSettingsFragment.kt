67ad7e54b0faf95a0ddfcbf28fb41b0ff531e196
Muraino Yakubu <murainoy@yahoo.com>
2023-08-12 19:36:02 +0100

    ExperimentalSettingsFragment

---
+++ app/src/main/java/com/muryno/muzic/component/settings/ui/ExperimentalSettingsFragment.kt
@@ -0,0 +1,20 @@
+package com.muryno.muzic.component.settings.ui
+
+import android.os.Bundle
+import com.kylecorry.andromeda.fragments.AndromedaPreferenceFragment
+import com.kylecorry.andromeda.sense.Sensors
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.shared.sensors.SensorService
+
+class ExperimentalSettingsFragment : AndromedaPreferenceFragment() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.experimental_preferences, rootKey)
+
+        val sensors = SensorService(requireContext())
+        val hasGyro = Sensors.hasGyroscope(requireContext())
+        val hasCompass = sensors.hasCompass()
+
+        preference(R.string.pref_experimental_metal_direction)?.isVisible = hasGyro && hasCompass
+    }
+}
