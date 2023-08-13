e5c3e0a53ab9544046b079d9de390f9c8ea4cd89
Muraino Yakubu <murainoy@yahoo.com>
2023-08-13 19:36:23 +0100

    FlashlightDiagnostic

---
+++ app/src/main/java/com/muryno/muzic/component/diagnostics/FlashlightDiagnostic.kt
@@ -0,0 +1,18 @@
+package com.muryno.muzic.component.diagnostics
+
+import android.content.Context
+import com.kylecorry.trail_sense.diagnostics.IDiagnostic
+import com.kylecorry.trail_sense.tools.flashlight.infrastructure.FlashlightSubsystem
+
+class FlashlightDiagnostic(private val context: Context) : IDiagnostic {
+
+    override fun scan(): List<DiagnosticCode> {
+        val issues = mutableListOf<DiagnosticCode>()
+
+        if (!FlashlightSubsystem.getInstance(context).isAvailable()) {
+            issues.add(DiagnosticCode.FlashlightUnavailable)
+        }
+
+        return issues
+    }
+}
