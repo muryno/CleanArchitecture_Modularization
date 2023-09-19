43a2ce638b1b872cb5d7325606672498cdf335e8
Muraino Yakubu <murainoy@yahoo.com>
2023-09-19 18:46:03 +0100

    PackageReplacedReceiver

---
+++ app/src/main/java/com/muryno/muzic/component/receivers/PackageReplacedReceiver.kt
@@ -0,0 +1,16 @@
+package com.muryno.muzic.component.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.kylecorry.trail_sense.receivers.TrailSenseServiceUtils
+
+class PackageReplacedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_MY_PACKAGE_REPLACED && context != null) {
+            Log.d("PackageReplacedReceiver", "Package replaced")
+            TrailSenseServiceUtils.restartServices(context, true)
+        }
+    }
+}
