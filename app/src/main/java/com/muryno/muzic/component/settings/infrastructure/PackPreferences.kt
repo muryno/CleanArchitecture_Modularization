2557fc8fb3bff31b96cc31e5b89ff101e728fb2f
Muraino Yakubu <murainoy@yahoo.com>
2023-09-19 19:23:16 +0100

    PackPreferences

---
+++ app/src/main/java/com/muryno/muzic/component/settings/infrastructure/PackPreferences.kt
@@ -0,0 +1,12 @@
+package com.muryno.muzic.component.settings.infrastructure
+
+import android.content.Context
+import com.kylecorry.andromeda.preferences.StringPreference
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.settings.infrastructure.PreferenceRepo
+
+class PackPreferences(context: Context): PreferenceRepo(context) {
+
+    var packSort by StringPreference(cache, context.getString(R.string.pref_pack_item_sort), "category")
+
+}
