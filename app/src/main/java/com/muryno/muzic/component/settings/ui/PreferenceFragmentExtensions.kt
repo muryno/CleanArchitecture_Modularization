e3058ef256638bed7f1cef782f4c1f8c10d9096a
Muraino Yakubu <murainoy@yahoo.com>
2023-09-28 12:24:44 +0100

    PowerSettingsFragment

---
+++ app/src/main/java/com/muryno/muzic/component/settings/ui/PreferenceFragmentExtensions.kt
@@ -0,0 +1,17 @@
+package com.muryno.muzic.component.settings.ui
+
+import androidx.annotation.IdRes
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
+import com.kylecorry.andromeda.core.tryOrNothing
+import com.kylecorry.andromeda.fragments.AndromedaPreferenceFragment
+import com.kylecorry.trail_sense.shared.navigateWithAnimation
+
+fun AndromedaPreferenceFragment.navigateOnClick(pref: Preference?, @IdRes action: Int) {
+    pref?.setOnPreferenceClickListener {
+        tryOrNothing {
+            findNavController().navigateWithAnimation(action)
+        }
+        true
+    }
+}
