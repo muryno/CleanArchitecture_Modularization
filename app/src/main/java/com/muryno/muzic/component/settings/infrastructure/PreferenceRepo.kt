b6d282e5c0a6b0d4ca9f138f29a5f3dc5f701fd1
Muraino Yakubu <murainoy@yahoo.com>
2023-10-03 10:25:27 +0100

    PreferenceRepo

---
+++ app/src/main/java/com/muryno/muzic/component/settings/infrastructure/PreferenceRepo.kt
@@ -0,0 +1,14 @@
+package com.muryno.muzic.component.settings.infrastructure
+
+import android.content.Context
+import androidx.annotation.StringRes
+import com.kylecorry.trail_sense.shared.preferences.PreferencesSubsystem
+
+abstract class PreferenceRepo(protected val context: Context) {
+
+    protected val cache by lazy { PreferencesSubsystem.getInstance(context).preferences }
+
+    protected fun getString(@StringRes id: Int): String {
+        return context.getString(id)
+    }
+}
