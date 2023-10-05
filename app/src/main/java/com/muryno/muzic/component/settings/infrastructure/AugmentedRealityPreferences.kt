f34d1a3050886d29a550cf6e36417ebe301e9ff3
Muraino Yakubu <murainoy@yahoo.com>
2023-10-05 11:25:55 +0100

    AugmentedRealityPreferences

--- app/src/main/java/com/muryno/muzic/component/settings/infrastructure/AugmentedRealityPreferences.kt
+++ app/src/main/java/com/muryno/muzic/component/settings/infrastructure/AugmentedRealityPreferences.kt
@@ -6,7 +6,6 @@ import com.kylecorry.andromeda.preferences.FloatPreference
 import com.kylecorry.sol.units.Distance
 import com.kylecorry.sol.units.DistanceUnits
 import com.kylecorry.trail_sense.R
-import com.kylecorry.trail_sense.settings.infrastructure.PreferenceRepo
 import com.kylecorry.trail_sense.shared.UserPreferences
 
 class AugmentedRealityPreferences(context: Context) : PreferenceRepo(context) {
