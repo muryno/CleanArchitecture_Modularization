8e94088f5ab695e645246be3fc46d123066e5ea7
Muraino Yakubu <murainoy@yahoo.com>
2023-10-19 12:28:04 +0100

    ExactAlarmPermissionChangedReceiver

--- app/src/main/java/com/muryno/muzic/component/receivers/ExactAlarmPermissionChangedReceiver.kt
+++ app/src/main/java/com/muryno/muzic/component/receivers/ExactAlarmPermissionChangedReceiver.kt
@@ -5,7 +5,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import com.kylecorry.trail_sense.receivers.RestartServicesCommand
 
 class ExactAlarmPermissionChangedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
