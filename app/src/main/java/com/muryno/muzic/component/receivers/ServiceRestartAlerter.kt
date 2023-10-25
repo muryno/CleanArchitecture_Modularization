84d964d3e41e2d9d70751ea9be88c9c1721d87de
Muraino Yakubu <murainoy@yahoo.com>
2023-10-25 21:18:52 +0100

    ServiceRestartAlerter

---
+++ app/src/main/java/com/muryno/muzic/component/receivers/ServiceRestartAlerter.kt
@@ -0,0 +1,36 @@
+package com.muryno.muzic.component.receivers
+
+import android.content.Context
+import com.kylecorry.andromeda.notify.Notify
+import com.kylecorry.trail_sense.main.NotificationChannels
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.main.MainActivity
+import com.kylecorry.trail_sense.shared.alerts.IDismissibleAlerter
+
+class ServiceRestartAlerter(private val context: Context) : IDismissibleAlerter {
+    override fun alert() {
+
+        val intent = MainActivity.pendingIntent(context)
+
+        val notification = Notify.status(
+            context,
+            NotificationChannels.CHANNEL_SERVICE_RESTART,
+            context.getString(R.string.restart_services_title),
+            context.getString(R.string.restart_services_message),
+            R.drawable.ic_alert,
+            group = NotificationChannels.GROUP_SERVICE_RESTART,
+            intent = intent,
+            autoCancel = true,
+            alertOnlyOnce = true,
+        )
+        Notify.send(context, NOTIFICATION_ID, notification)
+    }
+
+    override fun dismiss() {
+        Notify.cancel(context, NOTIFICATION_ID)
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 23759823
+    }
+}
