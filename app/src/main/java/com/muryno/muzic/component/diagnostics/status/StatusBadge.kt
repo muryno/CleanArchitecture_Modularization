3924d327c66aceb8f715240c6d8a94ddd6212bec
Muraino Yakubu <murainoy@yahoo.com>
2023-10-30 12:20:25 +0000

    StatusBadge

---
+++ app/src/main/java/com/muryno/muzic/component/diagnostics/status/StatusBadge.kt
@@ -0,0 +1,6 @@
+package com.muryno.muzic.component.diagnostics.status
+
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+
+data class StatusBadge(val name: String, @ColorInt val color: Int, @DrawableRes val icon: Int)
