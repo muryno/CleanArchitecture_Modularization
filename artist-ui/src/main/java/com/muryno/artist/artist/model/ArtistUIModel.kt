44e8d2c00a4de01cd7e00016d54394e87222da33
Frankdroid7 <ibukunoladipo2015@gmail.com>
2023-11-04 12:39:40 +0000

    edited artist ui model

--- artist-ui/src/main/java/com/muryno/artist/artist/model/ArtistUIModel.kt
+++ artist-ui/src/main/java/com/muryno/artist/artist/model/ArtistUIModel.kt
@@ -1,11 +1,11 @@
 package com.muryno.artist.artist.model
 
 data class ArtistUIModel(
+    val score: Int
     val id: String,
     val city: String,
     val name: String,
     val gender: String,
     val state: String,
     val description: String,
-    val score: Int
 )
