ede8a98f53be963b36f27cc6eef2c78723661027
Muraino Yakubu <murainoy@yahoo.com>
2023-11-10 18:18:47 +0000

    TimeChangeReceiver

---
+++ app/src/main/java/com/muryno/muzic/component/receivers/TimeChangeReceiver.kt
@@ -0,0 +1,22 @@
+package com.muryno.muzic.component.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.kylecorry.trail_sense.receivers.TrailSenseServiceUtils
+
+class TimeChangeReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val validIntentActions = listOf(
+            Intent.ACTION_TIMEZONE_CHANGED,
+            Intent.ACTION_TIME_CHANGED,
+            Intent.ACTION_DATE_CHANGED
+        )
+        if (validIntentActions.contains(intent?.action) && context != null) {
+            Log.d("TimeChangeReceiver", "Time changed")
+            TrailSenseServiceUtils.restartServices(context, true)
+        }
+    }
+}
