6a9e5915f13fdbc1b4011d32cea9168219731178
Muraino Yakubu <murainoy@yahoo.com>
2023-11-13 14:19:34 +0000

    RestartServicesCommand

--- app/src/main/java/com/muryno/muzic/component/receivers/RestartServicesCommand.kt
+++ app/src/main/java/com/muryno/muzic/component/receivers/RestartServicesCommand.kt
@@ -1,7 +1,6 @@
 package com.muryno.muzic.component.receivers
 
 import android.content.Context
-import com.kylecorry.trail_sense.receivers.TrailSenseServiceUtils
 import com.kylecorry.trail_sense.shared.commands.Command
 
 class RestartServicesCommand(private val context: Context, private val isBackground: Boolean): Command {
