94b88ec7def3147dd6792db07cb6f0385e91a8ed
Muraino Yakubu <murainoy@yahoo.com>
2023-11-14 18:19:50 +0000

    UnitSettingsFragment

---
+++ app/src/main/java/com/muryno/muzic/component/settings/ui/UnitSettingsFragment.kt
@@ -0,0 +1,15 @@
+package com.muryno.muzic.component.settings.ui
+
+import android.os.Bundle
+import com.kylecorry.andromeda.fragments.AndromedaPreferenceFragment
+import com.kylecorry.andromeda.sense.Sensors
+import com.kylecorry.trail_sense.R
+
+class UnitSettingsFragment : AndromedaPreferenceFragment() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.units_preferences, rootKey)
+        preference(R.string.pref_pressure_units)?.isVisible = Sensors.hasBarometer(requireContext())
+    }
+
+}
