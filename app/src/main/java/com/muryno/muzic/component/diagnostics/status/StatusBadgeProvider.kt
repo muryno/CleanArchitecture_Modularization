682bfcc25e9ffd8d2a55f3bab9e684cc98b0604a
Muraino Yakubu <murainoy@yahoo.com>
2023-12-03 12:47:54 +0000

    StatusBadgeProvider

--- app/src/main/java/com/muryno/muzic/component/diagnostics/status/StatusBadgeProvider.kt
+++ app/src/main/java/com/muryno/muzic/component/diagnostics/status/StatusBadgeProvider.kt
@@ -1,7 +1,5 @@
 package com.muryno.muzic.component.diagnostics.status
 
-import com.muryno.muzic.component.diagnostics.status.StatusBadge
-
 interface StatusBadgeProvider {
     fun getBadge(): StatusBadge
 }
