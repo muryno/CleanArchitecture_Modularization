49b607225ea6acfad1a938795ca803086cc717fd
Muraino Yakubu <murainoy@yahoo.com>
2024-01-06 12:27:44 +0000

    IBeaconPreferences

--- app/src/main/java/com/muryno/muzic/component/settings/infrastructure/IBeaconPreferences.kt
+++
@@ -1,8 +0,0 @@
-package com.muryno.muzic.component.settings.infrastructure
-
-import com.kylecorry.trail_sense.tools.beacons.infrastructure.sort.BeaconSortMethod
-
-interface IBeaconPreferences {
-    val showLastSignalBeacon: Boolean
-    var beaconSort: BeaconSortMethod
-}
