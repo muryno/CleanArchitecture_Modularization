c02971a58bb9394335de28d117d7216271b344c6
Muraino Yakubu <murainoy@yahoo.com>
2024-01-07 16:28:01 +0000

    IClinometerPreferences

--- app/src/main/java/com/muryno/muzic/component/settings/infrastructure/IClinometerPreferences.kt
+++
@@ -1,8 +0,0 @@
-package com.muryno.muzic.component.settings.infrastructure
-
-import com.kylecorry.sol.units.Distance
-
-interface IClinometerPreferences {
-    var lockWithVolumeButtons: Boolean
-    var baselineDistance: Distance?
-}
