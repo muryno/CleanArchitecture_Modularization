0f88cdcb8a5b487936e8fb20fb2ed9ff79146665
Muraino Yakubu <murainoy@yahoo.com>
2024-01-07 16:27:51 +0000

    ICameraPreferences

--- app/src/main/java/com/muryno/muzic/component/settings/infrastructure/ICameraPreferences.kt
+++
@@ -1,8 +0,0 @@
-package com.muryno.muzic.component.settings.infrastructure
-
-import com.muryno.muzic.component.settings.infrastructure.CameraPreferences
-
-interface ICameraPreferences {
-    val useZeroShutterLag: Boolean
-    val projectionType: CameraPreferences.ProjectionType
-}
