5a738202ed54495e8bf9a38585ca2698726cd32a
Muraino Yakubu <murainoy@yahoo.com>
2024-01-07 19:28:08 +0000

    ICompassPreferences

--- app/src/main/java/com/muryno/muzic/component/settings/infrastructure/ICompassPreferences.kt
+++
@@ -1,9 +0,0 @@
-package com.muryno.muzic.component.settings.infrastructure
-
-import com.kylecorry.trail_sense.shared.sensors.compass.CompassSource
-
-interface ICompassPreferences {
-    var compassSmoothing: Int
-    var useTrueNorth: Boolean
-    var source: CompassSource
-}
