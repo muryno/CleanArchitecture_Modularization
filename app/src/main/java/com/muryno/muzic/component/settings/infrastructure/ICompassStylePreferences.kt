31a633a4f018375754163a0c3593e5bba71be79c
Muraino Yakubu <murainoy@yahoo.com>
2024-01-08 15:28:16 +0000

    ICompassStylePreferences

--- app/src/main/java/com/muryno/muzic/component/settings/infrastructure/ICompassStylePreferences.kt
+++
@@ -1,9 +0,0 @@
-package com.muryno.muzic.component.settings.infrastructure
-
-interface ICompassStylePreferences {
-
-    val useLinearCompass: Boolean
-    val useRadarCompass: Boolean
-    val showDialTicksWhenNoCompass: Boolean
-
-}
