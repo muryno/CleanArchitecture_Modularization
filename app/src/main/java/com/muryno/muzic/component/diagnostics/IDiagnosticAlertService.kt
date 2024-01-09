f123d83491f10e6ca3352fbcc010bbb3de74ea7f
Muraino Yakubu <murainoy@yahoo.com>
2024-01-09 12:34:54 +0000

    IDiagnosticAlertService

--- app/src/main/java/com/muryno/muzic/component/diagnostics/IDiagnosticAlertService.kt
+++
@@ -1,7 +0,0 @@
-package com.kylecorry.trail_sense.diagnostics
-
-import com.muryno.muzic.component.diagnostics.DiagnosticCode
-
-interface IDiagnosticAlertService {
-    fun alert(code: DiagnosticCode)
-}
