8e5aaf1be2840d98265ac50cd76981ea2fd08dac
Muraino Yakubu <murainoy@yahoo.com>
2024-03-03 19:43:12 +0000

    uppdate name

--- artist-data/src/main/java/com/muryno/data/datasource/ArtistDataSource.kt
+++ artist-data/src/main/java/com/muryno/data/datasource/ArtistDataSource.kt
@@ -6,5 +6,5 @@ import com.muryno.data.model.ArtistDataModel
 
 interface ArtistDataSource {
     suspend fun getArtistListFromApi(artistName: String): List<ArtistDataModel>
-    suspend fun getArtistAlbumFromServer(artistId: String): List<ArtistAlbumDataModel>
+    suspend fun getArtistAlbumFromApi(artistId: String): List<ArtistAlbumDataModel>
 }
